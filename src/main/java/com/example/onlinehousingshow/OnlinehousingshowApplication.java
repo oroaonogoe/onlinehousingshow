91834c31b3d82606b63f699d268b8d6f383f52e3
oroaonogoe <ddedeepindde1@gmail.com>
2023-07-11 01:16:05 +0630

    initial

---
+++ src/main/java/com/example/onlinehousingshow/OnlinehousingshowApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinehousingshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinehousingshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinehousingshowApplication.class, args);
+	}
+
+}
